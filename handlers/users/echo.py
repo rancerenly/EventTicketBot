96cf80e921c0a909f6344e1e1e5393952dc67d2f
rancerenly <rancerenly@gmail.com>
2021-10-02 22:42:52 +0700

    before adding database

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -7,9 +7,7 @@ from loader import dp
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}")
+    await message.answer("Сам такой")
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
