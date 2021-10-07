479db867f6fd4ea70459b145d6d9d102df27a0b0
rancerenly <rancerenly@gmail.com>
2021-10-07 23:28:39 +0700

    added scheduler to send notify for events

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from aiogram import executor
 
-from loader import dp
+from loader import dp, scheduler
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -15,5 +15,6 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
+    scheduler.start()
     executor.start_polling(dp, on_startup=on_startup)
 
